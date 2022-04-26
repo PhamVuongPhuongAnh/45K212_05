9559c667c532dc0abaea8b61144326d6d56f0c64
DuongVietHuyHoang <191121521212@due.udn.vn>
2022-04-26 15:33:58 +0700

    update

---
+++ aodaiduongthi/slider.php
@@ -0,0 +1,14 @@
+    <!-- -----------------------SLlDER---------------------------------------------- -->
+    <section class="sliders">
+            <div class="aspect-ratio-169 sliders-img">
+                <img src="image/Banner1.png" alt="">
+                <img src="image/slider22.png" alt="">
+                <img src="image/slider123.png" alt="">
+            </div>
+            <div class="dot-container">
+                <div class="dot active"></div>
+                <div class="dot"></div>
+                <div class="dot"></div>
+           </div>
+    </section>
+    <!-- -----------------------------------slider----------------------- -->
